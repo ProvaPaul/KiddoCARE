35fae1390280226eeae1ac4a8cd2e07ce674271c
ProvaPaul <paul2007037@stud.kuet.ac.bd>
2025-05-20 13:53:39 +0600

    project setup completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function App() {
+
+  return (
+    <>
+      <button className='btn btn-primary'>Click Here</button>
+    </>
+  )
+}
+
+export default App
